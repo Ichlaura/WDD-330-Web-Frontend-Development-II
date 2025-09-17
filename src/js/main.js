4239258ca9b8fbdb2e4dc23a24dfd4e6a8d11835
laura nunez <laupao41@gmail.com>
2025-09-17 15:59:55 +0900

    commit lau week 2

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// Importar ProductData
+import ProductData from './js/ProductData.mjs';
+
+// Crear una instancia usando el archivo JSON de productos
+const dataSource = new ProductData('tents'); // 'tents' es el nombre del JSON sin extensión
